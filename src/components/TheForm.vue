6bae1b5bfd33a5819a02ae6107cc919012930ebe
Jovana Vlahovic <jovanavlahovic@gmail.com>
2021-06-14 19:34:41 +0200

    Reinstall tailwind and add some styling to header container

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
-    <header class="flex flex-wrap p-4">
-      <div class="form-loader">
+    <header class="flex flex-wrap p-4 items-center">
+      <div class="form-loader w-1/3">
         load
       </div>
-      <div >
+      <div class="w-2/3">
         <p class="state">New York State</p>
-        <h3>Request for Contact</h3>
+        <h1>Request for Contact</h1>
       </div>
     </header>
   </div>
@@ -20,18 +20,23 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  position: relative;
-  width: 60%;
-  margin: 0 auto;
-  top: 10%;
-}
+  .container {
+    position: relative;
+    width: 60%;
+    margin: 0 auto;
+    top: 10%;
+  }
 
-header {
-  background-color: #2094f3;
-}
+  header {
+    background-color: #2094f3;
+    height: 100px;
+  }
 
-h3 {
-  font-weight: normal;
-}
+  .state {
+    font-size: .8rem;
+  }
+
+  h1 {
+    font-size: 1.2rem;
+  }
 </style>
