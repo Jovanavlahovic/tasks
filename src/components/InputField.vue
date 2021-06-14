cd255fa545785fb557ed7111e09b62174aea05b3
Jovana Vlahovic <jovanavlahovic@gmail.com>
2021-06-14 22:44:54 +0200

    Underline border for input field animated

---
+++ src/components/InputField.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex flex-wrap p-4 md:py-6 h-[3.23rem] items-center">
+    <div class="order-number-checkbox px-4">
+      <div class="text-gray-400">{{ orderNumber }}</div>
+    </div>
+    <div class="form-control pl-7 md:pl-10 flex flex-col w-4/5 relative">
+      <label class="text-gray-400 text-xs md:text-sm"> {{ label }} </label>
+      <input :type="inputType" class="focus:outline-none transition" @input="onInput($event)">
+      <div class="border"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    orderNumber: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    inputType: {
+      type: String,
+      required: true
+    }
+  },
+   methods: {
+    onInput(value) {
+       console.log(value);
+     }
+   }
+}
+</script>
+
+<style scoped>
+  input {
+    border-bottom: 1px solid rgb(156, 163, 175);
+    color: rgb(156, 163, 175);
+  }
+
+  .border {
+    position: absolute;
+    width: 0%;
+    height: 0%;
+    bottom: 0;
+    background-color: #2094f3;
+    padding: 0 0;
+    transition: height 0.3s, width 0.3s 0.1s;
+  }
+
+  input:focus {
+    border: none;
+  }
+
+  input:focus ~ .border {
+    width: 90%;
+    height: 2px;
+  }
+
+</style>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,21 +1,56 @@
 <template>
   <div class="container">
-    <header class="flex flex-wrap p-4 items-center">
-      <div class="form-loader w-1/3">
-        load
+    <header class="flex flex-wrap p-4 md:pl-6 items-center shadow-md rounded">
+      <div class="loader-container flex justify-center items-center">
+        <circle-progress
+            class="loader"
+            :percent="40"
+            :size="50"
+            :border-width="5"
+            :border-bg-width="5"
+            :empty-color="emptyColor"
+            fill-color="white"
+            :hide-text="false"
+        />
+        <p class="absolute progress-value">50%</p>
       </div>
-      <div class="w-2/3">
+      <div class="pl-4 md:pl-6">
         <p class="state">New York State</p>
         <h1>Request for Contact</h1>
       </div>
     </header>
+    <main >
+      <form class="shadow-md rounded">
+        <input-field input-type="text" label="First Name" order-number="1" v-model="firstName"/>
+        <input-field input-type="text" label="Last Name" order-number="2" v-model="lastName"/>
+        <input-field input-type="text" label="Email Adress" order-number="3" v-model="email"/>
+      </form>
+    </main>
   </div>
 </template>
 
 <script>
+import CircleProgress from "vue3-circle-progress";
+import InputField from "./InputField";
 
 export default {
   name: "Form",
+  components: {
+    CircleProgress,
+    InputField
+  },
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+    }
+  },
+  computed: {
+    emptyColor() {
+      return "rgba(255,255,255, 0.3)";
+    }
+  }
 }
 </script>
 
@@ -33,10 +68,50 @@ export default {
   }
 
   .state {
-    font-size: .8rem;
+    font-size: .7rem;
   }
 
   h1 {
-    font-size: 1.2rem;
+    font-size: 1rem;
   }
+
+  .loader {
+    width: 40px;
+    height: 40px;
+    position: absolute;
+  }
+
+
+  .progress-value {
+    font-size: .7rem;
+  }
+
+  @media(max-width: 860px) {
+    .container {
+      width: 80%;
+    }
+  }
+
+  @media (min-width: 860px) {
+    .loader {
+      width: 60px;
+      height: 60px;
+    }
+    .progress-value {
+      font-size: .9rem;
+    }
+
+    .state {
+      font-size: .8rem;
+    }
+
+    h1 {
+      font-size: 1.2rem;
+    }
+  }
+
+  form {
+    background-color: #fff;
+  }
+
 </style>
