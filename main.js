75966d073fc8066b5162df5916ee1f8eef0e0406
Jovana Vlahovic <jovanavlahovic@gmail.com>
2021-06-15 10:58:34 +0200

    Validate password function

---
+++ main.js
@@ -0,0 +1,14 @@
+function checkPassword(password) {
+    let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+    let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
+
+    if(strongPassword.test(password)) {
+        return "Strong";
+    }
+
+    if(mediumPassword.test(password)) {
+        return'Medium';
+    }
+
+    return "Weak";
+}
